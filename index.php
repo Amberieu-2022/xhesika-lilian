dd7a90db3a1597a28ca51e0a34e29cedc48ae669
LilianD5 <l.diaz@codeur.online>
2022-02-17 13:36:55 +0100

    Header

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600&family=Dancing+Script:wght@400;500;600&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <link rel="icon" size="16x16" type="image/png" href="./img/icone/icon.png">
+    <link rel="stylesheet" href="style.css">
+    <title>Beauty & Soul | Salon de beauté à Lyon 6ème</title>
+</head>
+<body>
+    <header>
+        <div class="row-limit-size flex">
+            <nav class="nav-1">
+                <ul class="main-menu">
+                    <li><a href="#">Soin du visage</a></li>
+                    <li><a href="#">Soin du corps</a></li>
+                </ul>
+            </nav>
+            <h1 id="h1" ><a href="#">Beauty & Soul</a></h1>
+            <nav class="nav-2">
+                <ul class="main-menu">
+                    <li><a href="#">Massage</a></li>
+                    <li><a href="#">Promo</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+</body>
+</html>
