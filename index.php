58c77999cddb91f02a9e75ea539f452bf06b5414
XhesikaMilaqi <jessy.99milaqi@gmail.com>
2022-02-21 14:06:36 +0100

    prestation+promo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,24 +10,183 @@
     <link rel="stylesheet" href="style.css">
     <title>Beauty & Soul | Salon de beauté à Lyon 6ème</title>
 </head>
+
 <body>
     <header>
         <div class="row-limit-size flex">
             <nav class="nav-1">
                 <ul class="main-menu">
-                    <li><a href="#">Soin du visage</a></li>
-                    <li><a href="#">Soin du corps</a></li>
+                    <li><a href="#">Visage</a></li>
+                    <li><a href="#">Corps</a></li>
                 </ul>
             </nav>
-            <h1 id="h1" ><a href="#">Beauty & Soul</a></h1>
+            <h1 id="h1"><a href="#">Beauty & Soul</a></h1>
             <nav class="nav-2">
                 <ul class="main-menu">
                     <li><a href="#">Massage</a></li>
                     <li><a href="#">Promo</a></li>
                 </ul>
             </nav>
+            <div id="icone-burger"><img src="./img/icone/burgerM.png" alt="menu déroulant" width="30px"></div>
+            <nav id="menu-b" class="burger-menu">
+                <ul class="main-menu">
+                    <li><a href="#">Visage</a></li>
+                    <li><a href="#">Corps</a></li>
+                    <li><a href="#">Massage</a></li>
+                    <li><a href="#">Promo</a></li>
+                </ul>
+                <figure id="logo-menu">
+                    <img src="../img/logo1.png" alt="La logo de salon" width="250px">
+                </figure>
+                <div id="icones">
+                    <img src="./img/icone/facebook.png" alt="icone facebook" width="20px">
+                    <img src="./img/icone/youtube.png" alt="icone youtube" width="20px">
+                    <img src="./img/icone/insta.png" alt="icone instagram" width="20px">
+
+                </div>
+            </nav>
+
+
         </div>
+
     </header>
-    
+
+    <!-- __________________MAIN____________________ -->
+    <main>
+        <div id="info" class="flex">
+            <div id="icone-info">
+                <img src="./img/icone/facebook.png" alt="icone facebook" width="35px">
+                <img src="./img/icone/youtube.png" alt="icone youtube" width="35px">
+                <img src="./img/icone/insta.png" alt="icone instagram" width="35px">
+            </div>
+            <p id="adresse">00 Rue Prenom Nom Lyon 6</p>
+            <p id="horaires">Mardi - Vendredi : 10h-19h<br>Samedi : 9h - 20h</p>
+        </div>
+        <figure id="big-logo">
+            <img src="./img/logo1.png" alt="la logo de notre institut/ Feuille" width="480px">
+        </figure>
+        <h2>BEAUTY. LUXE. ELEGANCE.</h2>
+        <div id="prestation">
+            <div>
+                <div id="first-b">
+                    <h3>Bienvenue</h3>
+                    <p class="bienvenue-para">DANS NOTRE SALON DE BIEN-ETRE</p>
+                </div>
+                <div class="bienvenue">
+                    <img src="./img/contenu/bienvenue.jpg" alt="Un image qui montre quelques utils de salon de beauté.">
+                    <div id="right">
+                        <p class="intro">Bienvenue chez Beauty &<br> Soul, ou vous trouverez des<br>
+                            jeunes gens passionnés et<br> talentueux à votre service.<br>
+                            Nous vous invitons à explorer<br> nos services exceptionels<br>
+                            et nous découvrir.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="bienvenue">
+                <div class="left">
+                    <h4>soin du visage</h4>
+
+                    <p class="soin">Un teint frais et healthy commence<br> avec une peau en bonne santé.<br>
+                        Beauty & soul propose des soins<br>
+                        visage adaptés à tous les types de<br>
+                        peaux. Peau sensible, grasse, mixte <br>ou sèche, faites votre
+                        choix parmi les<br> soins, démaquillants, ou sérums qui<br>
+                        redonneront de l'éclat à votre peau.
+                    </p>
+                </div>
+                <img src="./img/contenu/visage.jpg" alt="Image de soin de visage.">
+
+            </div>
+            <div class="bienvenue">
+                <img src="./img/contenu/corps.jpg" alt="Image de soin du corps.">
+                <div>
+                    <h5>soin du corps</h5>
+                    <p class="corp">
+                        Prendre soin de son corps est<br> indispensable pour préserver toute <br>la souplesse et
+                        l’élasticité de la peau.<br> Nettoyant, gommage et crème<br> hydratante, Beauty & Soul vous<br>
+                        offre le meilleur pour prendre <br>soin quotidiennement de votre <br>corps. Retrouvez
+                        instantanément <br>une peau belle, douce et soyeuse !
+                    </p>
+                </div>
+            </div>
+            <div class="bienvenue">
+                <div>
+                    <h6>massage</h6>
+                    <p class="soin">Découvrez les Massages Secrets <br>
+                        et Personnalisés du plus beau <br>Spa chez Beauty & Soul.
+                        <br> Tous nos Massages sont réalisés<br> à l'huile chaude parfumée,<br>dans une
+                        salle de soin double et <br> chaleureuse éclairée à la<br>
+                        lumière des bougies. <br> Large choix parmi: Massage <br>Relaxant, Massage Tonifiant, <br>Massage en Profondeur,
+                        <br> Massage Drainant, etc ...
+                    </p>
+                </div>
+                <img src="./img/contenu/massage.jpg" alt="Image de massages.">
+
+            </div>
+
+
+
+        </div>
+
+        <p class="offre">offre promo</p>
+        <div id="promo">
+
+            <div>
+                <div class="promo-1">
+                    <img src="./img/promo/promo1.jpg" alt="coffret cadeau">
+                    <img src="./img/promo/promo33.png" alt="promo -33%" class="img-promo">
+                </div>
+                <p class="titre-promo">Coffret Beauty & Soul </p>
+            </div>
+            <div>
+                <div class="promo-1">
+                    <img src="./img/promo/promo2.jpg" alt="carte cadeau">
+                    <img src="./img/promo/promo20.png" alt="promo -33%" class="img-promo">
+                </div>
+                <p class="titre-promo">Gift Card Beauty & Soul </p>
+
+            </div>
+            <div>
+                <div class="promo-1">
+                    <img src="./img/promo/promo3.jpg" alt="Offre de Saint Valentin">
+                    <img src="./img/promo/promo50.png" alt="promo -33%" class="img-promo">
+                </div>
+                <p class="titre-promo">Offre de la Saint Valentin<br> Massage en Duo </p>
+
+            </div>
+
+        </div>
+
+    </main>
+
+
+    <!-- ________________FOOTER______________________ -->
+    <footer>
+        <div id="contact">
+
+            <img src="./img/logo2.png" alt="la logo de l'institut" class="logo-footer">
+
+            <p class="b-logo">Beauty & Soul</p>
+            <div id="icone-footer">
+                <img src="./img/icone/facebook.png" alt="icone facebook" width="35px">
+                <img src="./img/icone/youtube.png" alt="icone youtube" width="35px">
+                <img src="./img/icone/insta.png" alt="icone instagram" width="35px">
+            </div>
+        </div>
+        <div id="navlist">
+            <ul>
+                <li><a href="#">Plan du site</a></li>
+                <li><a href="#"> Mention légale</a></li>
+                <li><a href="#"> Conditions d’utilisation</a></li>
+                <li><a href="#"> Conditions générales de vente</a></li>
+                <li><a href="#"> Données personnelles et cookies</a></li>
+            </ul>
+        </div>
+        <p class="copy"><a href="#">Copyright © 2021. Tous droits réservés.</a></p>
+    </footer>
+
+    <script src="./img/main.js"></script>
 </body>
+
 </html>
